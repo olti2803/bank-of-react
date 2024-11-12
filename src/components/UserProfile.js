eb792d38eccc73a99e7a215c460020d84cc23c6a
Olti <oltigjoni2003@gmail.com>
2024-11-11 20:00:43 -0500

    Add and test User Profile component with React Router v6 syntax

---
+++ src/components/UserProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserProfile() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Username: Joe Smith</p>
+      <p>Member Since: 11/22/99</p>
+    </div>
+  );
+}
+
+export default UserProfile;
